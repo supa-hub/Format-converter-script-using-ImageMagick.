86917283a8c7a15fa4d9d86a1844a58c2ecfdbc7
supa-hub <bilcutomi@gmail.com>
2024-05-23 17:12:09 +0300

    FIrst commit to repo.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Picture format converter using ImageMagick.
+
+## Description
+This is a simple script that goes through your folder and searches for the pictures of the given format. After that it <br /> 
+uses the ImageMagick command tool to convert them into the user's specified format.  <br />
+
+This makes it possible for to store more <br /> 
+efficient image formats on your phone, but to easily convert them when storing them on your computer. <br />
+
+After ImageMagick has converted all the files, the script moves them into a folder named "converted_files", <br />
+this folder is usually created in the same folder that the original pictures were. <br />
+If the folder already exists, the code created the folder into the scripts parent folder. <br /> <br />
+
+NOTE: the script prints out the converted picture folder path into the terminal. <br /> <br />
+
+All the libraries used in the code are part of the python's standard library. <br /> <br />
+
+
+## Compatibility
+
+Development and testing platforms: <br /> <br/>
+
+OS: Windows 10  22H2. <br />
+CPU: intel i7-1165g7 <br />
+code version: python 3.11 ( can work with  at least python 3.9 or newer ). <br />
+ImageMagick version: 7.1.0-35 Q16-HDRI x64. <br />
---
+++ heif_to_python.py
@@ -0,0 +1,153 @@
+import os
+import glob
+import shutil
+import subprocess
+import threading
+import os
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
+
+
+result_available = threading.Event()
+
+class convert:
+    def __init__(self):
+        self.pythonfiles_directory = filedialog.askdirectory()
+        
+        try:
+            converted_pictures_path = os.path.join(self.pythonfiles_directory, "converted_pictures")
+            y_file = os.makedirs(converted_pictures_path)
+        except Exception:
+            # if the converted pictures folder cannot be create in the given (or it already exists), then the code will create the folder in the current directory.
+            print("folder to store the converted pictures already exists, so the script will not create another one")
+            self.pythonfiles_directory = os.getcwd()
+
+
+
+        self.picture_list = []
+        self.jpg_picture_list = []
+        self.pictures_already_converted = []
+        self.jpg_already_converted = []
+
+        
+        
+        for xx in glob.glob("*.heic"):
+            self.picture_list.append(xx)
+
+        for zz in glob.glob("*.jpg"):
+            self.jpg_already_converted.append(zz)
+
+        for yy in glob.glob(r"converted_pictures\*.jpg", recursive=True):
+            for_yyy = "converted_pictures\{}"
+            for_yyy = for_yyy.format("")
+            yyy = yy.replace(for_yyy,"")
+            #print(yyy)
+            self.pictures_already_converted.append(yyy)
+
+        self.length_picture_list = len(self.picture_list)/4
+        self.st_length_picture_list = int(round(self.length_picture_list,0))
+        self.nd_length_picture_list = int(round(self.length_picture_list,0))
+        self.rd_length_picture_list = int(round(self.length_picture_list,0))
+        self.th_length_picture_list = int(round(self.length_picture_list,0))
+
+
+        self.whole_count = self.st_length_picture_list + self.nd_length_picture_list + self.rd_length_picture_list + self.th_length_picture_list
+
+        if self.whole_count != len(self.picture_list):
+            self.th_length_picture_list = self.th_length_picture_list + (len(self.picture_list)- self.whole_count)
+
+
+
+
+    def convert_picture(self, count_for_multithreading):
+        if count_for_multithreading == 0:
+            counting_amount = self.st_length_picture_list
+        if count_for_multithreading == 1:
+            counting_amount = self.nd_length_picture_list
+        if count_for_multithreading == 2:
+            counting_amount = self.rd_length_picture_list
+        if count_for_multithreading == 3:
+            counting_amount = self.th_length_picture_list
+            
+        for xx_convert_count in range(counting_amount):
+            new_name = self.picture_list[xx_convert_count + count_for_multithreading*self.st_length_picture_list]
+            picture_name = new_name.replace(" ","")
+            os.rename(self.picture_list[xx_convert_count + count_for_multithreading*self.st_length_picture_list], picture_name)
+            
+            self.filename = self.picture_list[xx_convert_count + count_for_multithreading*self.st_length_picture_list]
+            self.filename2 = self.filename.split(".")
+            
+            try:
+                self.filename4 = self.filename2[0].split(" ")
+                self.filename5 = str(self.filename4[0]+self.filename4[1])
+                self.filename3 = self.filename5 + ".jpg"
+            except Exception:
+                self.filename3 = self.filename2[0] + ".jpg"
+
+            self.jpg_picture_list.append(self.filename3)
+
+
+            if self.filename3 in self.pictures_already_converted:
+                print(f"\033[93m file already converted: \033[0m {self.filename2[0]}")
+                self.jpg_picture_list.remove(self.filename3)
+                #result_available.set()
+                pass
+            elif self.filename3 in self.jpg_already_converted:
+                print(f"\033[93m file already converted: \033[0m {self.filename3}")
+                #result_available.set()
+                pass
+            else:
+                system_string = r"cd {} & magick convert {} {} & exit"   # the command that will execute the picture converter
+                system_format = system_string.format(self.pythonfiles_directory, str(self.filename), self.filename3) # we go into the given file path and convert the pictures.
+                #print(system_format_list)
+                print(f"file being converted: {self.filename}")
+                process = subprocess.Popen(system_format, shell=True)
+                process.wait()
+        result_available.set()
+
+    def move_picture(self):
+        self.string_to_move_picture = "{}\converted_pictures"
+        self.string_to_move_picture2 = self.string_to_move_picture.format(self.pythonfiles_directory)  # the directory where the converted pictures will be
+
+        for xx_convert_count in self.jpg_picture_list:
+            try:
+                #print("juu")
+                shutil.move(xx_convert_count, self.string_to_move_picture2)
+                
+            except Exception:
+                print("\033[91m a converted file could not be moved into the target folder, the converted file is in the same folder as the original one \033[0m")
+                print(f" \033[91m the file that couldn't be moved: \033[0m {xx_convert_count}")
+                
+
+        print(f"\033[92m converted files have been successfully moved to the folder at directory: \033[0m {self.string_to_move_picture2}") 
+
+
+        
+
+        
+convert_picture = convert()
+t1 = threading.Thread(target=convert_picture.convert_picture(0))
+t2 = threading.Thread(target=convert_picture.convert_picture(1))
+t3 = threading.Thread(target=convert_picture.convert_picture(2))
+t4 = threading.Thread(target=convert_picture.convert_picture(3))
+
+
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+
+
+result_available.wait()
+t5 = threading.Thread(target=convert_picture.move_picture())
+t5.start()
+
+    
+
+
+
+
+
